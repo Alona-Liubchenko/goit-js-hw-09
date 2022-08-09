4da1e54a28ad0f935fd9624bb5c3b27ab578a01d
Alona-Liubchenko <98355708+Alona-Liubchenko@users.noreply.github.com>
2022-08-09 21:03:47 +0000

    Deploying to gh-pages from @ Alona-Liubchenko/goit-js-hw-09@b72bb2a1132319fb8ca7f10626556aeb665a9597 🚀

---
+++ 01-color-switcher.226c0a8f.js
@@ -0,0 +1,2 @@
+document.querySelector("button[data - start]"),document.querySelector("button[data - stop]");
+//# sourceMappingURL=01-color-switcher.226c0a8f.js.map
---
+++ 01-color-switcher.423bc0de.js
@@ -0,0 +1,2 @@
+document.querySelector("button[data - start]"),document.querySelector("button[data - stop]");
+//# sourceMappingURL=01-color-switcher.423bc0de.js.map
