b72bb2a1132319fb8ca7f10626556aeb665a9597
Alona-Liubchenko <aylin888@rambler.ru>
2022-08-10 00:03:19 +0300

    h3

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,8 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const refs = {
+  startButton: document.querySelector('button[data - start]'),
+  stopButton: document.querySelector('button[data - stop]'),
+};
