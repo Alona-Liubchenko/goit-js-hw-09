afa306f4882aff5ed8db1bce2e17f622fde3cc40
Alona-Liubchenko <aylin888@rambler.ru>
2022-08-12 00:28:28 +0300

    h5

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -3,13 +3,24 @@ function getRandomHexColor() {
 }
 
 const refs = {
-  startBtn: document.querySelector('button[data - start]'),
-  stopBtn: document.querySelector('button[data - stop]'),
-  bodyRef: document.body,
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+  colorBody: document.querySelector('body'),
 };
 refs.startBtn.addEventListener('click', onstartBtn);
 refs.stopBtn.addEventListener('click', onstopBtn);
 let timerId = null;
-function onstartBtn() {}
 
-function onstopBtn() {}
+function onstartBtn() {
+  timerId = setInterval(() => {
+    refs.colorBody.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  refs.startBtn.disabled = true;
+  console.log(refs.startBtn);
+}
+
+function onstopBtn() {
+  clearInterval(timerId);
+  refs.startBtn.disabled = false;
+  console.log(refs.stopBtn);
+}
